88c0b9fed13f13b029829e2adb85c78be66e386e
Almir <contact.almir@gmail.com>
2019-04-11 00:29:23 -0500

    created night mode state functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from '../Search/Search';
 import Headlines from '../Headlines/Headlines';
 import './App.css';
 
 function App() {
+  const [isNightMode, setMode] = useState(false);
+  const mode = () => setMode(!isNightMode);
+
   return (
-    <div>
+    <div className={isNightMode ? 'night' : ''}>
       <div className="wrapper">
+        <button onClick={mode}>Mode</button>
         <div className="container">
           <Search />
           <Headlines />
