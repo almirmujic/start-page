cd7c880ac851258666d608deca1fc812074eb6a2
Almir <contact.almir@gmail.com>
2019-04-11 16:10:29 -0500

    created bookmarks component + stylesheet

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bookmark from '../Bookmark/Bookmark';
+import './Bookmarks.css';
+
+function Bookmarks() {
+  return (
+    <div className="bookmarks">
+      <Bookmark />
+    </div>
+  );
+}
+
+export default Bookmarks;
