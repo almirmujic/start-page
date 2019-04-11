02832e2e11094a31dbd872fa12f16dca31c9a4fe
Almir <contact.almir@gmail.com>
2019-04-11 00:29:37 -0500

    created headlines component

--- src/components/Headlines/Headlines.jsx
+++ src/components/Headlines/Headlines.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Article from '../Article/Article';
 
 function Headlines() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Article />
+    </div>
+  );
 }
 
 export default Headlines;
