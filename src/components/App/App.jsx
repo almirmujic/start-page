1ff8ca34c83cfa083b847b303f652aca7db9cac9
Almir <contact.almir@gmail.com>
2019-04-11 16:21:40 -0500

    added state change on button click + button styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Search from '../Search/Search';
 import Headlines from '../Headlines/Headlines';
+import Bookmarks from '../Bookmarks/Bookmarks';
 import './App.css';
 
 function App() {
@@ -10,9 +11,12 @@ function App() {
   return (
     <div className={isNightMode ? 'night' : ''}>
       <div className="wrapper">
-        <button onClick={mode}>Mode</button>
+        <button className={isNightMode ? 'night' : ''} onClick={mode}>
+          Mode
+        </button>
         <div className="container">
           <Search />
+          <Bookmarks />
           <Headlines />
         </div>
       </div>
