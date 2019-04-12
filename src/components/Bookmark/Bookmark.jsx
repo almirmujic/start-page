6ba5dd8ca935502c7d8ad2a183afe479994003e9
Almir <contact.almir@gmail.com>
2019-04-11 19:14:20 -0500

    passing props into each bookmark container

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Bookmark.css';
 
-function Bookmark() {
-  return <div>Individual Bookmark here</div>;
+function Bookmark({ icon, title }) {
+  return (
+    <div className="bookmark">
+      <div>{icon}</div>
+      <p>{title}</p>
+    </div>
+  );
 }
 
 export default Bookmark;
