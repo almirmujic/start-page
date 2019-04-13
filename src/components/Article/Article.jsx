42670a96abb260797f0f0ca3cdd30e65b940b369
Almir <contact.almir@gmail.com>
2019-04-12 23:12:58 -0500

    linked Article stylesheet to Article component + added filler content

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './Article.css';
 
 function Article() {
-  return <div>Article component</div>;
+  return (
+    <div className="Article">
+      <p>
+        Article component Article componentArticle componentArticle
+        componentArticle component Article componentArticle componentArticle
+        component
+      </p>
+    </div>
+  );
 }
 
 export default Article;
