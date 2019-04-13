ec8552b9c89a7512beae2fc55f38738b356bdbe2
Almir <contact.almir@gmail.com>
2019-04-12 23:15:22 -0500

    linked greeting stylesheet to greeting component + added Hello string

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Greeting.css';
 
 const info = {
   name: 'Almir'
 };
 
 const Greeting = () => {
-  return <h1>{info.name}</h1>;
+  return <h1>Hello, {info.name}.</h1>;
 };
 
 export default Greeting;
