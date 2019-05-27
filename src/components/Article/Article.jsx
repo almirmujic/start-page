eb8642b2a9200749b96794d95e6c30cf25d34b8f
Almir <contact.almir@gmail.com>
2019-05-27 18:29:40 -0500

    in progress

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './Article.css';
+// import React, { useState, useEffect } from 'react';
+// import './Article.css';
 
-function Article() {
-  const [person, setPerson] = useState(null);
+// function Article() {
+//   const [person, setPerson] = useState(null);
 
-  async function fetchData() {
-    const response = await fetch('https://api.randomuser.me/');
-    const person = await response.json();
-    const [item] = person.results;
-    setPerson(item);
-  }
+//   async function fetchData() {
+//     const response = await fetch('https://api.randomuser.me/');
+//     const person = await response.json();
+//     const [item] = person.results;
+//     setPerson(item);
+//   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
-  return (
-    <a href="/" className="Article">
-      {person && person.name.first}
-    </a>
-  );
-}
+//   return (
+//     <a href="/" className="Article">
+//       {person && person.name.first}
+//     </a>
+//   );
+// }
 
-export default Article;
+// export default Article;
--- src/components/Headlines/Headlines.jsx
+++ src/components/Headlines/Headlines.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './Headlines.css';
-import Article from '../Article/Article';
+// import React from 'react';
+// import './Headlines.css';
+// import Article from '../Article/Article';
 
-function Headlines() {
-  return (
-    <div className="Headlines">
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-    </div>
-  );
-}
+// function Headlines() {
+//   return (
+//     <div className="Headlines">
+//       <Article />
+//       <Article />
+//       <Article />
+//       <Article />
+//     </div>
+//   );
+// }
 
-export default Headlines;
+// export default Headlines;
